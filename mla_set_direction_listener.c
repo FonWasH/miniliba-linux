5cf24e14c8de502ab93753283643e7592cf979fd
fonwash <fonwash@gmail.com>
2025-05-07 17:24:14 +0200

    remove 42header

--- mla_set_direction_listener.c
+++ mla_set_direction_listener.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   mla_set_direction_listener.c                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: juperez <juperez@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/06 17:43:21 by juperez           #+#    #+#             */
-/*   Updated: 2025/05/06 17:44:17 by juperez          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "mla.h"
 
 void	mla_set_direction_listener(void *engine_ptr, float x, float y, float z)
